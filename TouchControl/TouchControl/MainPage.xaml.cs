8e3df0501180773fe601d40b8b08eaff2ad2ef25
Daniel Villalba <villalbaweb@gmail.com>
2024-03-11 18:39:07 -0600

    Refactoring, now the control moves as expected

--- TouchControl/TouchControl/MainPage.xaml.cs
+++ TouchControl/TouchControl/MainPage.xaml.cs
@@ -1,24 +1,64 @@
-﻿namespace TouchControl
+﻿namespace TouchControl;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    double tempx = 0;
+    double tempy = 0;
+
+    public MainPage()
     {
-        int count = 0;
+        InitializeComponent();
+    }
 
-        public MainPage()
+    private void AddLabel_Clicked(object sender, EventArgs e)
+    {
+        var label = new Label()
         {
-            InitializeComponent();
-        }
+            Text = "This is a label",
+            BackgroundColor = Colors.LightGray,
+            Padding = 10
+        };
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        var panGesture = new PanGestureRecognizer();
+        panGesture.PanUpdated += PanGestureRecognizer_PanUpdated;
+
+        label.GestureRecognizers.Add(panGesture);
+
+        ParentLayout.Children.Add(label);
+    }
+
+    private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+        var label = sender as Label;
+        switch (e.StatusType)
         {
-            count++;
+            case GestureStatus.Started:
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    tempx = label.TranslationX;
+                    tempy = label.TranslationY;
+                }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                break;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            case GestureStatus.Running:
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    label.TranslationX = e.TotalX + tempx;
+                    label.TranslationY = e.TotalY + tempy;
+                }
+                else if (Device.RuntimePlatform == Device.Android)
+                {
+                    label.TranslationX += e.TotalX;
+                    label.TranslationY += e.TotalY;
+                }
+
+                break;
+            case GestureStatus.Completed:
+                tempx = label.TranslationX;
+                tempy = label.TranslationY;
+
+                break;
         }
     }
 
